b7ff59d2467cbbab342e8a81da53fb57f0debde9
amagyei <kwame.na@hotmail.com>
2024-10-20 23:47:26 +0000

    task

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -1,9 +1,13 @@
-#!/usr/bin python3
-
+#!/usr/bin/python3
+"""
+Pascal's Triangle 
+"""
 def create_list(i):
+    '''Returns a list of 1s'''
     return [1] * (i +1)
 
 def triangle_populator(triangle):
+    '''Populates each list of the triangle with correct value'''
     triangle[0][0] = 1
     for i in range(1, len(triangle)):
         for j in range(1, len(triangle[i]) - 1):
@@ -14,6 +18,7 @@ def triangle_populator(triangle):
     
 
 def pascal_triangle(n):
+    ''' Makes the triangle'''
     triangle = []
     for i in range(n):
         triangle.append(create_list(i))
